7c0aeb75dc5ef1c84051739eee550ed28d9803bd
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2019-11-04 14:28:16 +0700

    add mvp

---
+++ aws-signv4-apigateway/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import requests
+from requests_aws4auth import AWS4Auth
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        error_message = "usage: aws-signv4-apigateway <rest-api-endpoint>\n\naws-signv4-apigateway: error: too few arguments"
+        sys.exit(error_message)
+    else:
+
+        auth = AWS4Auth(
+            os.environ.get("AWS_ACCESS_KEY_ID"),
+            os.environ.get("AWS_SECRET_ACCESS_KEY"),
+            'ap-southeast-1',
+            'execute-api',
+            session_token=os.environ.get("AWS_SESSION_TOKEN")
+        )
+
+        endpoint = sys.argv[1]
+        response = requests.get(endpoint, auth=auth)
+
+        print(response.text)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aws-signv4-apigateway",
+    version="0.0.1",
+    packages=find_packages(),
+    url="https://github.com/rafikurnia/aws-signv4-apigateway",
+    license="Apache License 2.0",
+    author="Rafi Kurnia Putra",
+    author_email="rafi.putra@traveloka.com",
+    description="Sign your request to invoke Amazon API Gateway REST API Endpoint with Signature Version 4",
+    install_requires=[
+        "requests-aws4auth==0.9",
+    ],
+    python_requires=">=3",
+    entry_points={
+        "console_scripts": [
+            "aws-signv4-apigateway=aws-signv4-apigateway.main:main"
+        ],
+    }
+)
