a292615848d660d3828c4f260d8f1eb1f51c60da
Rafi Kurnia Putra <rafi.putra@traveloka.com>
2019-11-06 11:31:50 +0700

    add long description and bump to 0.1.0

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="aws-signv4-apigateway",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     url="https://github.com/rafikurnia/aws-signv4-apigateway",
     license="Apache License 2.0",
     author="Rafi Kurnia Putra",
     author_email="rafi.putra@traveloka.com",
     description="Sign your request to invoke Amazon API Gateway REST API Endpoint with Signature Version 4",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         "requests-aws4auth==0.9",
     ],
